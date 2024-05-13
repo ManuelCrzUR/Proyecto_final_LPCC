ea8edab6a50f5c176698a76f24b80eac7238c51d
Manuel Cruz Garrote <manuels.cruz@urosario.edu.co>
2024-05-13 17:10:37 -0500

    Se termina de programar las 3 reglas, queda pendiente la visualizacion de las formulas

--- desarrollo/desarrollo_interfaz/interfaz.py
+++ desarrollo/desarrollo_interfaz/interfaz.py
@@ -21,14 +21,6 @@ def visualizar_horario(nombre, horarios):
         tabla.auto_set_font_size(False)
         tabla.set_fontsize(12)
         tabla.scale(1.2, 1.8)
-        
-        c = 1
-        for i in disponibilidad:
-            if not i:
-                for j in range(len(datos[0])):
-                    tabla.get_celld()[(c, j)].set_facecolor('red')
-            c += 1
-
 
         # Agregar texto fuera de la tabla
         plt.text(0.5, 1.1, f'Horario para monitorias {nombre}',
@@ -40,13 +32,6 @@ def visualizar_horario(nombre, horarios):
         # Mostrar la figura
         fig.savefig("horario" + str(nombre) + ".png")
   
-def colorear_ocupados():
-        c = 1
-        for i in disponibilidad:
-            if not i:
-                for j in range(len(datos[0])):
-                    tabla.get_celld()[(c, j)].set_facecolor('red')
-            c += 1
 
           
 from codificadores import *
@@ -56,11 +41,8 @@ profesores = ['Norma', 'Edgar', 'Edwin']
 # lista de horarios:
 horarios = ['09:00 - 11:00', '11:00 - 13:00', '13:00 - 15:00 ']
     
-def horario_profesor(lista_profesores, lista_horarios, Nlista_horarios = []):
-    for p in range(len(lista_profesores) - 1):
-        for Np in range(len(Nlista_horarios) - 1):
-            
-            visualizar_horario(p, lista_horarios)
+def horario_profesor(lista_profesores, lista_horarios):
+    for p in lista_profesores:  
+        visualizar_horario(p, lista_horarios)
 
 horario_profesor(profesores, horarios)
-
